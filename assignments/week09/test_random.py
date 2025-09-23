5ae97db4c6795c4b4203cf7f3e963b323d9e1061
Aunnop Somocha <aunnop.s@ku.th>
2025-09-23 11:52:44 +0700

    week9

--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,22 @@
 import random
 
+#refactor != debug
+
 def test_random():
     random_number = random.randint(1, 100)
+
+    guess_number = input("What is your guess number?: ")
+    guess_number = int(guess_number)
+
+    if random_number == guess_number:
+        print("Congratulation")
+    
+    elif random_number < guess_number:
+        print("Too much")
+
+    elif random_number > guess_number:
+        print("Too low")
+
     print(random_number)
     
 test_random()
